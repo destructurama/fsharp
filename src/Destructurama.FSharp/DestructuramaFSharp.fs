7e81142656f2691f86b2d40de61997b418b84f07
Chet Husk <chusk3@gmail.com>
2020-01-18 13:34:50 -0600

    tweak outputs somewhat for tuples and options. need to figure out struct options for typeshape

--- src/Destructurama.FSharp/DestructuramaFSharp.fs
+++ src/Destructurama.FSharp/DestructuramaFSharp.fs
@@ -1,11 +1,11 @@
 ﻿// Copyright 2015 Destructurama Contributors, Serilog Contributors
-// 
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-// 
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -21,32 +21,152 @@ open System.Reflection
 
 #if NETSTANDARD2_0
 open TypeShape
+open TypeShape.Core
+open System.Collections.Concurrent
 #endif
 
 module Impls =
-    let destructureNetstandard16(value: obj, factory: ILogEventPropertyValueFactory, result: byref<LogEventPropertyValue>): bool = 
+
+#if NETSTANDARD1_6
+
+    type System.Type with
+        member x.IsGenericType = x.GetTypeInfo().IsGenericType
+        member x.GetProperty(name, flags: BindingFlags) = x.GetTypeInfo().GetProperty(name, flags)
+
+    let destructureNetstandard16(value: obj, factory: ILogEventPropertyValueFactory, result: byref<LogEventPropertyValue>): bool =
         let inline cpv obj = factory.CreatePropertyValue(obj, true)
         let inline lep (n: PropertyInfo) (v:obj) = LogEventProperty(n.Name, cpv v)
 
         match value.GetType() with
         | t when FSharpType.IsTuple t ->
-            result <- SequenceValue(value |> FSharpValue.GetTupleFields |> Seq.map cpv)
+            let fields = FSharpValue.GetTupleFields value
+            let properties = 
+                match fields with
+                | [||] -> Seq.empty
+                | [| f |] -> Seq.singleton (LogEventProperty("Item", cpv f))
+                | fields -> fields |> Seq.mapi (fun i f -> LogEventProperty(sprintf "Item%d" (i + 1), cpv f))
+            result <- StructureValue(properties)
             true
         // TODO: support for Maps and Sets? Why do Lists here when surely some IEnumerable-binder can handle them?
         | t when t.IsConstructedGenericType && t.GetGenericTypeDefinition() = typedefof<List<_>> ->
             let objEnumerable = value :?> System.Collections.IEnumerable |> Seq.cast<obj>
             result <- SequenceValue(objEnumerable |> Seq.map cpv)
             true
+        | t when t.IsGenericType && t.GetGenericTypeDefinition() = typedefof<option<obj>> -> 
+            let optionTy = typedefof<Option<obj>>.MakeGenericType [| t.GenericTypeArguments.[0] |]
+            // dirty hack because options have CompilationRepresentation.Null on the none case
+            let isNone v = obj.ReferenceEquals(v, null)
+
+            let valueGetter =
+                let valueMember = optionTy.GetProperty("Value", BindingFlags.Public ||| BindingFlags.Instance)
+                fun v -> valueMember.GetValue(v)
+
+            if isNone value
+            then
+                result <- StructureValue(Seq.empty, "None")
+                true
+            else
+                result <- StructureValue(Seq.singleton (LogEventProperty("Some", (factory.CreatePropertyValue(valueGetter value, true)))))
+                true
         | t when FSharpType.IsUnion t ->
             let case, fields = FSharpValue.GetUnionFields(value, t)
             let properties = (case.GetFields(), fields) ||> Seq.map2 lep
             result <- StructureValue(properties, case.Name)
             true
         | _ -> false
+#endif
 
 #if NETSTANDARD2_0
-    let destructureNetstandard20(value: obj, factory: ILogEventPropertyValueFactory, result: byref<LogEventPropertyValue>): bool = 
-        destructureNetstandard16(value, factory, &result)
+    type Destructurer<'t> = 't -> ILogEventPropertyValueFactory -> bool * LogEventPropertyValue
+
+    // let inline getV< ^a when ^a : (member GetValue:  obj -> obj)> (info: ^a) (v: obj) =
+    //     ( (^a): (member GetValue: obj -> obj) (info, v))
+
+    let private propGetter (m: MemberInfo) =
+        match m.MemberType with
+        | MemberTypes.Field -> Some ((m :?> FieldInfo).GetValue)
+        | MemberTypes.Property -> Some ((m :?> PropertyInfo).GetValue)
+        | _ -> None
+
+    let private getReaders (fields: IShapeReadOnlyMember []) =
+        fields
+        |> Array.choose (fun field -> match  propGetter field.MemberInfo with Some f -> Some(field.Label, f) | None -> None)
+
+    let readStructure tag fields v (f: ILogEventPropertyValueFactory) =
+        let values =
+            fields
+            |> Array.map (fun (name, getter) ->
+                            let prop = f.CreatePropertyValue(getter v, true)
+                            LogEventProperty(name, prop))
+            |> Seq.ofArray
+        StructureValue(values, tag) :> LogEventPropertyValue
+
+    type Holder =
+        static member makeDestructurer<'t> (): Destructurer<'t> =
+            let wrap (thing: 'a) = unbox<'t> thing
+
+            match shapeof<'t> with
+            | Shape.Tuple tup ->
+                let reader = readStructure null (getReaders tup.Elements)
+                fun v f -> true, reader v f
+            | Shape.FSharpOption o ->
+                let optionTy = typedefof<Option<obj>>.MakeGenericType [| o.Element.Type |]
+                // dirty hack because options have CompilationRepresentation.Null on the none case
+                let isNone v = obj.ReferenceEquals(v, null)
+
+                let value =
+                    let valueMember = optionTy.GetProperty("Value", BindingFlags.Public ||| BindingFlags.Instance)
+                    fun v -> valueMember.GetValue(v)
+
+                fun v f ->
+                    if isNone v
+                    then
+                        true, StructureValue(Seq.empty, "None") :> _
+                    else
+                        true, StructureValue(Seq.singleton (LogEventProperty("Some", (f.CreatePropertyValue(value v, true))))) :> _
+            // | Shape.FSharpUnion u ->
+            //     let caseMap =
+            //         u.UnionCases
+            //         |> Array.map (fun u -> u.CaseInfo.Name, readStructure u.CaseInfo.Name (getReaders u.Fields))
+            //         |> Map.ofArray
+            //     fun v f ->
+            //         let case, _ = FSharpValue.GetUnionFields(v, v.GetType())
+            //         match Map.tryFind case.Name caseMap with
+            //         | Some r -> true, r v f
+            //         | None -> false, null
+            | Shape.FSharpRecord r ->
+                let reader = readStructure null (getReaders r.Fields)
+                fun v f -> true, reader v f
+            | _ ->
+                fun v f -> (false, null)
+
+
+            // | Shape.FSharpList l ->
+    let private destructurerDict = ConcurrentDictionary<System.Type, Destructurer<obj>>()
+
+    let private destructurerForType: System.Type -> Destructurer<obj> =
+        let destructureMeth = typeof<Holder>.GetMethod("makeDestructurer", BindingFlags.Static ||| BindingFlags.Public)
+        fun t ->
+            // the second part of the func
+            let secondFunc = FSharpType.MakeFunctionType(typeof<ILogEventPropertyValueFactory>, typeof<bool * LogEventPropertyValue>)
+            let secondInvoke = secondFunc.GetMethod("Invoke", BindingFlags.Public ||| BindingFlags.Instance)
+            let firstFunc = FSharpType.MakeFunctionType(t, secondFunc)
+            let firstInvoke = firstFunc.GetMethod("Invoke", BindingFlags.Public ||| BindingFlags.Instance)
+            let destructurer = destructureMeth.MakeGenericMethod([| t |]).Invoke(null, [||])
+            fun (value: obj) (factory: ILogEventPropertyValueFactory) ->
+                // TODO: fsharpfunc.adapt?
+                let firstStep = firstInvoke.Invoke(destructurer, [| value |])
+                secondInvoke.Invoke(firstStep, [| factory |]) :?> bool * LogEventPropertyValue
+
+    let destructureNetstandard20(value: obj, factory: ILogEventPropertyValueFactory, result: byref<LogEventPropertyValue>): bool =
+        let destTy = value.GetType()
+        let destructurer = destructurerDict.GetOrAdd(destTy, System.Func<_,_>(destructurerForType))
+        match destructurer value factory with
+        | true, r ->
+            result <- r
+            true
+        | false, _ ->
+            false
 #endif
 
 type public FSharpTypesDestructuringPolicy() =
--- test/Destructurama.FSharp.Test/Tests.fs
+++ test/Destructurama.FSharp.Test/Tests.fs
@@ -2,28 +2,83 @@ module Tests
 
 open Xunit
 open Destructurama.FSharp
+open Serilog.Events
+open Serilog.Core
+
+[<CustomEquality; NoComparison>]
+type SValue =
+| Scalar of obj
+| Sequence of SValue seq
+| Structure of tag: string option * values: (string * SValue) seq
+  interface System.IEquatable<SValue> with
+     member x.Equals(y) =
+        match x, y with
+        | Scalar l, Scalar r ->
+            l = r
+        | Sequence l, Sequence r ->
+            Seq.zip l r
+            |> Seq.forall (fun (l, r) -> (l :> System.IEquatable<SValue>).Equals r)
+        | Structure(lt, lv) , Structure(rt, rv) ->
+            lt = rt
+            && Seq.zip lv rv |> Seq.forall (fun ((ln, lv), (rn, rv)) -> ln = rn && (lv :> System.IEquatable<SValue>).Equals rv)
+        | l, r -> false
+
+
+let rec toSValue (l: LogEventPropertyValue) =
+    match l with
+    | :? ScalarValue as s -> Scalar s.Value
+    | :? SequenceValue as s -> s.Elements |> Seq.map toSValue |> Sequence
+    | :? StructureValue as s ->
+        Structure(
+            (match s.TypeTag with | null -> None | str -> Some str),
+            s.Properties |> Seq.map (fun p -> p.Name, toSValue p.Value)
+        )
+    | t -> failwithf "unknown serilog value type %s" (t.GetType().FullName)
+
+// let rec fromSValue s =
+//     match s with
+//     | Scalar o -> ScalarValue o :> LogEventPropertyValue
+//     | Sequence s -> SequenceValue(s |> Seq.map fromSValue) :> _
+//     | Structure(t, vs) ->
+//         let vs = vs |> Seq.map (fun (name, v) -> LogEventProperty(name, fromSValue v))
+//         StructureValue(vs, (match t with | None -> null | Some s -> s)) :> _
 
 // dummy converter that always returns true
-let alwaysTrueConverter = 
-    { new Serilog.Core.ILogEventPropertyValueFactory with 
-        member __.CreatePropertyValue(data, shouldDestructure) = Serilog.Events.ScalarValue(true) :> _ }
+let alwaysScalarConverter =
+    { new Serilog.Core.ILogEventPropertyValueFactory with
+        member __.CreatePropertyValue(data, shouldDestructure) = Serilog.Events.ScalarValue(data) :> _ }
 
-let policy = 
+let policy =
     FSharpTypesDestructuringPolicy() :> Serilog.Core.IDestructuringPolicy
 
-let inline doDestructuring data = 
-    match policy.TryDestructure(data, alwaysTrueConverter) with
-    | true, _property -> ()
+let inline doDestructuring data expected =
+    match policy.TryDestructure(data, alwaysScalarConverter) with
+    | true, property ->
+        let actual = toSValue property
+        if (expected :> System.IEquatable<SValue>).Equals actual
+        then ()
+        else
+            failwithf "Expected %A to match %A" actual expected
     | false, _ -> failwithf "was unable to destructure an object of type %s" (data.GetType().FullName)
 
-[<Fact>]
-let ``can destructure tuple`` () = doDestructuring (1,2)
+let structure tag (props: (string * LogEventPropertyValue) seq) =
+    StructureValue(props |> Seq.map LogEventProperty, tag)
+let scalar v = ScalarValue v :> LogEventPropertyValue
 
 [<Fact>]
-let ``can destructure struct tuple`` () = doDestructuring (struct (1,2))
+let ``can destructure tuple`` () = doDestructuring (1,2) (Structure(None, seq { yield "Item1", Scalar 1
+                                                                                yield "Item2", Scalar 2 }))
+
+#if NETCOREAPP2_2
+[<Fact>]
+let ``can destructure struct tuple`` () = doDestructuring (struct (1,2)) (Structure(None, seq { yield "Item1", Scalar 1
+                                                                                                yield "Item2", Scalar 2}))
+#endif
 
 [<Fact>]
-let ``can destructure union`` () = doDestructuring (Some "thing")
-    
+let ``can destructure option`` () = doDestructuring (Some "thing") (Structure(None, seq { yield "Some", Scalar "thing" }))
+
+#if NETCOREAPP2_2
 [<Fact>]
-let ``can destructure struct union`` () = doDestructuring (ValueSome "thing")
+let ``can destructure struct option`` () = doDestructuring (ValueSome "thing") (Structure(None, seq { yield "Some", Scalar "thing"}))
+#endif
